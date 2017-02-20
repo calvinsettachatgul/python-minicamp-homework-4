c3efba5d7b6dfa7936761a233bcc83dd5b4efb75
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-19 22:00:20 -0800

    fix a typo

--- movie_server.py
+++ movie_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, sqlite3, request
 
-app = FLask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-	render_template('home.html')
+	return render_template('home.html')
