de717be0104e6b9b03b96d75d7337e00e940381c
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-20 12:48:50 -0800

    get search working and now on to movies

--- movie_server/movie_server.py
+++ movie_server/movie_server.py
@@ -14,29 +14,40 @@ def new():
 	return render_template('new.html')
 
 @app.route('/movie', methods = ['POST'])
-def create():	
+def movie():	
 	connection = sqlite3.connect("database.db")
 	cursor = connection.cursor()
 	if request.method == "POST":
 		try:
-			print("try block")
-			title = request.form("title")
-			cursor.execute("INSERT INTO movies (\"name\"), values(\"{}\")".format( title))
-			cursor.commit()
-			message = "Record Added"
+			# print("try block")
+			title = request.form["title"]
+			# print(title)	
+			query =  "INSERT INTO movies  values (\"{}\")".format( title)
+			# print (query)
+			cursor.execute( 'INSERT INTO movies values(\"{}\")'.format( title) )
+			connection.commit()
+			message = "good"
 		except:
-			cursor.rollback()
-			print("something wrong")
-			message = "Something Wrong"
+			connection.rollback()
+			message = "no good"
 		finally:		
-				
 			connection.close()
-			print("done")
-			return render_template('result.html', message = message)
+		return render_template('result.html', message = message)
 
-@app.route('/movies')
+@app.route('/movies', methods = ['GET'])
 def movies():
-	return jsonify("all the movies")
+	connection = sqlite3.connect("database.db")
+	cursor = connection.cursor()
+	if request.method == "GET":
+		try:
+			all_movies_cursor = cursor.excecute('SELECT * FROM movies')
+			# print( all_movies_cursor )
+			# results = all_movies_cursor.fetchall()
+		except:
+			return "something wrong"
+		finally:
+			connection.close()
+		return jsonify( results)
 
 @app.route('/search', methods = ['POST'])
 def search():
