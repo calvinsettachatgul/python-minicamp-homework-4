ae6439b720740ae7fcbef6dd522d217521329401
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-20 00:13:30 -0800

    result.html

--- movie_server/movie_server.py
+++ movie_server/movie_server.py
@@ -14,8 +14,25 @@ def new():
 	return render_template('new.html')
 
 @app.route('/movie', methods = ['POST'])
-def create():
-	return render_template('home.html')
+def create():	
+	connection = sqlite3.connect("database.db")
+	cursor = connection.cursor()
+	if request.method == "POST":
+		try:
+			print("try block")
+			title = request.form("title")
+			cursor.execute("INSERT INTO movies (\"name\"), values(\"{}\")".format( title))
+			cursor.commit()
+			message = "Record Added"
+		except:
+			cursor.rollback()
+			print("something wrong")
+			message = "Something Wrong"
+		finally:		
+				
+			connection.close()
+			print("done")
+			return render_template('result.html', message = message)
 
 @app.route('/movies')
 def movies():
