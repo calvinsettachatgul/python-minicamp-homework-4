5c15c11a446f3e920c7917025d2924dfdd960f9e
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-02-19 22:20:12 -0800

    some changes

--- movie_server/movie_server.py
+++ movie_server/movie_server.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, sqlite3, request
-
+from flask import Flask, render_template, request, jsonify
+import sqlite3
 app = Flask(__name__)
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/new', methods = ['GET'])
+def new():
+	return render_template('new.html')
+
+@app.route('/movie', methods = ['POST'])
+def create():
+	return render_template('home.html')
+
+@app.route('/movies')
+def movies():
+	return jsonify("all the movies")
